9a882de6b8282597ff8ce70b034576701eba55ab
Vitor Fratti <vifratti@gmail.com>
2023-02-22 17:16:30 -0300

    Adicionei

---
+++ script.js
@@ -0,0 +1,71 @@
+document.getElementById('um__button').addEventListener('click', () => {
+    document.getElementById('um').classList.add('animate__bounceOutLeft')
+    setTimeout(() => {
+        document.getElementById('um').classList.add('none')
+        document.getElementById('dois').classList.remove('none')
+        document.getElementById('dois').classList.add('animate__bounceInRight')
+    }, 300)
+})
+
+let porcentagem = 0;
+
+document.getElementById('porcentagem').innerHTML = porcentagem
+
+function aumentarPorcentagem(event) {
+    if(event.target.innerHTML === '+') {
+        porcentagem += 10
+        document.getElementById('porcentagem').innerHTML = porcentagem
+        document.getElementById('teamo').style.opacity = porcentagem + '%'
+    } else if(event.target.innerHTML === '-') {
+        porcentagem -= 10
+        document.getElementById('porcentagem').innerHTML = porcentagem
+        document.getElementById('teamo').style.opacity = porcentagem + '%'
+    }
+}
+
+document.getElementById('dois__button').addEventListener('click', () => {
+    if(porcentagem >= 100) {
+        document.getElementById('dois').classList.add('animate__bounceOutLeft')
+        setTimeout(() => {
+            document.getElementById('dois').classList.add('none')
+            document.getElementById('tres').classList.remove('none')
+            document.getElementById('tres').classList.add('animate__bounceInRight')
+        }, 300)
+    } else {
+        alert('Amor insuficiente 😪')
+    }
+})
+
+let resposta = '12/09'
+
+function respostaCorreta(event) {
+    if(event.target.innerHTML === resposta) {
+        event.target.classList.add('right')
+        document.getElementById('tres').classList.add('animate__bounceOutLeft')
+        setTimeout(() => {
+            document.getElementById('tres').classList.add('none')
+            document.getElementById('quatro').classList.remove('none')
+            document.getElementById('quatro').classList.add('animate__bounceInRight')
+        }, 300)
+    } else {
+        event.target.classList.add('wrong')
+        alert('BLOCK 🚨🚨🚨🚨🚨')
+    }
+}
+
+function next() {
+    document.getElementById('quatro').classList.add('animate__bounceOutLeft')
+    setTimeout(() => {
+        document.getElementById('quatro').classList.add('none')
+        document.getElementById('cinco').classList.remove('none')
+        document.getElementById('cinco').classList.add('animate__bounceInRight')
+    }, 300)
+    setTimeout(() => {
+        document.getElementById('cinco').classList.add('animate__bounceOutLeft')
+    }, 3000)
+    setTimeout(() => {
+        document.getElementById('cinco').classList.add('none')
+        document.getElementById('seis').classList.remove('none')
+        document.getElementById('seis').classList.add('animate__bounceInRight')
+    }, 3300)
+}
