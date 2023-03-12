0cbd75ec7f0c9b99c2bdf95eb79151927856419d
Vitor Fratti <vifratti@gmail.com>
2023-03-12 15:39:51 -0300

    asiuhaskubjd

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
+const textoUm = document.getElementById('texto-um')
+
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){   
+      
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 100 * i)
+
+  });
+}
+
+typeWrite(textoUm)
+
+const textoDois = document.getElementById('texto-dois')
+
 document.getElementById('um__button').addEventListener('click', () => {
     document.getElementById('um').classList.add('animate__bounceOutLeft')
     setTimeout(() => {
         document.getElementById('um').classList.add('none')
         document.getElementById('dois').classList.remove('none')
         document.getElementById('dois').classList.add('animate__bounceInRight')
+        typeWrite(textoDois)
     }, 300)
 })
 
@@ -29,6 +48,8 @@ function aumentarPorcentagem(event) {
     }
 }
 
+const textoTres = document.getElementById('texto-tres')
+
 document.getElementById('dois__button').addEventListener('click', () => {
     if(porcentagem >= 100) {
         document.getElementById('dois').classList.add('animate__bounceOutLeft')
@@ -36,12 +57,14 @@ document.getElementById('dois__button').addEventListener('click', () => {
             document.getElementById('dois').classList.add('none')
             document.getElementById('tres').classList.remove('none')
             document.getElementById('tres').classList.add('animate__bounceInRight')
+            typeWrite(textoTres)
         }, 300)
     } else {
         alert('Amor Insuficiente 😪')
     }
 })
 
+const textoQuatro = document.getElementById('texto-quatro')
 let resposta = '12/09'
 
 function respostaCorreta(event) {
@@ -52,6 +75,7 @@ function respostaCorreta(event) {
             document.getElementById('tres').classList.add('none')
             document.getElementById('quatro').classList.remove('none')
             document.getElementById('quatro').classList.add('animate__bounceInRight')
+            typeWrite(textoQuatro)
         }, 300)
     } else {
         event.target.classList.add('wrong')
@@ -59,14 +83,19 @@ function respostaCorreta(event) {
     }
 }
 
+const textoCinco = document.getElementById('texto-cinco')
+
 function next() {
     document.getElementById('quatro').classList.add('animate__bounceOutLeft')
     setTimeout(() => {
         document.getElementById('quatro').classList.add('none')
         document.getElementById('cinco').classList.remove('none')
         document.getElementById('cinco').classList.add('animate__bounceInRight')
+        typeWrite(textoCinco)
     }, 300)
 }
+
+const textoSeis = document.getElementById('texto-seis')
 
 document.getElementById('cinco__button').addEventListener('click', () => {
     document.getElementById('cinco').classList.add('animate__bounceOutLeft')
@@ -74,6 +103,7 @@ document.getElementById('cinco__button').addEventListener('click', () => {
         document.getElementById('cinco').classList.add('none')
         document.getElementById('seis').classList.remove('none')
         document.getElementById('seis').classList.add('animate__bounceInRight')
+        typeWrite(textoSeis)
     }, 300)
 })
 
